a746ab2034d1837a4305aeaba4e448911838b170
“William <william.iso@hotmail.com>
2023-09-28 22:05:47 -0300

    Aula01

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Api.Donation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Get";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Post";
+        }
+
+        [HttpPut]
+        public string Put()
+        {
+            return "Put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Delete";
+        }
+
+
+    }
+}
---
+++ Controllers/TipoProdutoController.cs
@@ -0,0 +1,38 @@
+﻿using Fiap.Api.Donation.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Api.Donation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TipoProdutoController : ControllerBase
+    {
+        [HttpGet]
+        public IList<TipoProdutoModel> Get()
+        {
+            return new List<TipoProdutoModel>()
+            {
+                new TipoProdutoModel()
+                {
+                    TipoProdutoId = 1,
+                    Nome = "Celular"
+                },
+                new TipoProdutoModel() {
+                    TipoProdutoId = 2,
+                    Nome = "Televisor"
+                }
+            };
+        }
+
+        [HttpGet("{id:int}")]
+        public TipoProdutoModel Get([FromRoute] int id)
+        {
+            return new TipoProdutoModel()
+            {
+                TipoProdutoId = 1,
+                Nome = "Celular"
+            };
+        }
+    }
+}
